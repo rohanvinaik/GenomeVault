63737e5e8614c11c00dfa37a889f9054e95ef0a8
Rohan Vinaik <rohanpvinaik@gmail.com>
2025-07-29 20:10:54 -0400

    test(pipelines): add profiler test

---
+++ tests/pipelines/test_profile.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from genomevault.pipelines.profile import profile_dataframe
+
+def test_profile_dataframe_basic():
+    df = pd.DataFrame({"a":[1,2,2], "b":["x",None,"y"]})
+    prof = profile_dataframe(df)
+    assert prof["row_count"] == 3
+    assert "a" in prof["columns"] and "b" in prof["columns"]
